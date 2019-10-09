8909d0d3e29341e6ae122d4d0f92ba90a18e54cd
Alex <4546371+xitonix@users.noreply.github.com>
2019-10-09 16:30:07 +1100

    Upgrade to v2.0.0

--- trubka.rb
+++ trubka.rb
@@ -1,8 +1,8 @@
 class Trubka < Formula
   desc "Generic protobuf message Kafka consumer"
   homepage "https://github.com/xitonix/trubka"
-  url "https://github.com/xitonix/trubka/releases/download/v1.0.1/trubka_darwin-v1.0.1.tar.gz"
-  sha256 "e7a363137c04f53a9135cf03d688ceff5408c21fe31e149b6981be4050aaa34a"
+  url "https://github.com/xitonix/trubka/releases/download/v2.0.0/trubka_darwin-v2.0.0.tar.gz"
+  sha256 "11302701fbf58214b8a55e40f43e2dbacc8c5e381a1a91e086b9334225e69379"
 
   def install
     bin.install "trubka"
