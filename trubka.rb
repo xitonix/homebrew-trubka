d5be356b5556420f318abe31aff98a6e9f60de10
Alex <4546371+xitonix@users.noreply.github.com>
2020-01-09 08:50:15 +1100

    Description updated

--- trubka.rb
+++ trubka.rb
@@ -1,5 +1,5 @@
 class Trubka < Formula
-  desc "Generic protobuf message Kafka consumer"
+  desc "A CLI tool for Kafka"
   homepage "https://github.com/xitonix/trubka"
   url "https://github.com/xitonix/trubka/releases/download/v2.0.4/trubka_darwin-v2.0.4.tar.gz"
   sha256 "3f7a28cf814069ffb19f139923c44b130646693af19228064477c0a539ba2eac"
