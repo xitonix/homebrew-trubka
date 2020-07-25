d5bcb174d1669f808107a3a6dabb0b05d4a4bd03
Alex Golshani <4546371+xitonix@users.noreply.github.com>
2020-07-25 23:26:02 +1000

    Update to v3.1.2

--- trubka.rb
+++ trubka.rb
@@ -1,8 +1,8 @@
 class Trubka < Formula
   desc "A CLI tool for Apache Kafka"
   homepage "https://github.com/xitonix/trubka"
-  url "https://github.com/xitonix/trubka/releases/download/v3.1.1/trubka_darwin-v3.1.1.tar.gz"
-  sha256 "8d275101b062fd162c33b8971ad7e012b5c639839662a7b4dfb0a2bcaac42412"
+  url "https://github.com/xitonix/trubka/releases/download/v3.1.2/trubka_darwin-v3.1.2.tar.gz"
+  sha256 "79abc92310c5427e6e8c34db87c98b5d85675c21f4e149055bdda748427d1ca4"
 
   def install
     bin.install "trubka"
