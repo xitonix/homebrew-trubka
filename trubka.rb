4292cd662cd46c19e7bb64230262619024d4553e
Alex Golshani <4546371+xitonix@users.noreply.github.com>
2020-09-18 11:31:52 +1000

    Added version attribute

--- trubka.rb
+++ trubka.rb
@@ -3,6 +3,7 @@ class Trubka < Formula
   homepage "https://github.com/xitonix/trubka"
   url "https://github.com/xitonix/trubka/releases/download/v3.2.0/trubka_darwin-v3.2.0.tar.gz"
   sha256 "c10002b119195c99ebaff1e8bc214ac85ea23b143881e93eb24573d433709899"
+  version "v3.2.0"
 
   def install
     bin.install "trubka"
