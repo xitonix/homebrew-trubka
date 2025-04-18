df8e2e6cb2309231be1056f46406738877b38b52
runner <runner@Mac-1744980472302.local>
2025-04-18 12:59:27 +0000

    trubka v40.0.0

--- trubka.rb
+++ trubka.rb
@@ -1,9 +1,9 @@
 class Trubka < Formula
   desc "A CLI tool for Apache Kafka"
   homepage "https://github.com/xitonix/trubka"
-  url "https://github.com/xitonix/trubka/releases/download/v3.2.1/trubka_3.2.1_darwin_amd64.tar.gz"
-  sha256 "2305d4c0a7427203ff6ea13c277d1bb3a9a5555c87bd85b391e946660ea205d9"
-  version "v3.2.1"
+  url "https://github.com/xitonix/trubka/releases/download/v40.0.0/trubka_40.0.0_darwin_amd64.tar.gz"
+  sha256 "93533268144f302da722ecf4471fcb14e67e0efbb41ab7f3bd1611d60e8c89a4"
+  version "v40.0.0"
 
   def install
     bin.install "trubka"
